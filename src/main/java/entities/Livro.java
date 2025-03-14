0e312b0bcf6c2fe721eb96575e65a3ae68c41c39
PedroJuanOfc <pedrosaraivaofc@gmail.com>
2025-03-13 21:32:30 -0300

    Criação da classe Livro, seu construtor, e metodos Getters e Setters.

---
+++ src/main/java/entities/Livro.java
@@ -0,0 +1,69 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Livro {
+    private String id;
+    private String titulo;
+    private Autor autor;
+    private boolean disponivel;
+    private LocalDate dataCadastro;
+    private LocalDate dataAtualizacao;
+
+    public Livro(String id, String titulo, Autor autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+        this.dataCadastro = LocalDate.now();
+        this.dataAtualizacao = LocalDate.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public LocalDate getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
